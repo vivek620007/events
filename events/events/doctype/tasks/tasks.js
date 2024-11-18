7914e497222fac79e54edd3fd9ca523d943e7e55
Vivek Kumar <vivekkumar6200168007@gmail.com>
2024-11-18 14:14:15 +0530

    changes

--- events/events/doctype/tasks/tasks.js
+++ events/events/doctype/tasks/tasks.js
@@ -1,8 +1,9 @@
 // Copyright (c) 2024, Vivek and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Tasks", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Tasks", {
+	refresh(frm) {
+        console.log("kjhkjhjhj");
+        
+	},
+});
