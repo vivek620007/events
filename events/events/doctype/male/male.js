d5eb5880289988acd39ca036bae94076aebb7ed2
vivek620007 <vivekkumar6200168007@gmail.com>
2024-11-19 11:26:46 +0530

    changes

--- events/events/doctype/male/male.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2024, Vivek and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("Male", {
-// 	refresh(frm) {
-
-// 	},
-// });
--- events/events/doctype/male/male.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2024, Vivek and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-
-class Male(Document):
-	pass
